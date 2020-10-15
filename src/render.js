a89d5b1e0e44cf1855add56a21d24e792a0e4fe8
tedu14 <thalison.eduardo14@gmail.com>
2020-10-15 16:46:51 -0300

    feat: add a frogger class

--- src/render.js
+++ src/render.js
@@ -37,3 +37,26 @@ const maxParticles = 300;
 const ripplesArray = []
 const carsArray = []
 const logsArray = []
+
+
+//Frogger
+class Froggger{
+    constructor(){
+        this.spriteWidth = 250;
+        this.spriteHeight = 250;
+        this.width = this.spriteWidth/5;
+        this.height = this.spriteHeight/5;
+        this.x = canvas.width/2 - this.width/2;
+        this.y = canvas.height - this.height - 40;
+        this.moving = false;
+        this.frameX = 0;
+        this.frameY = 0;
+    }
+
+    draw(){
+        ctx3.fillStyle = 'green';
+        ctx3.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const frogger = new Froggger()

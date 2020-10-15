da92a128f03144308af3b3719cb2c221414ed916
tedu14 <thalison.eduardo14@gmail.com>
2020-10-15 16:23:54 -0300

    feat: add a initial canvas setup

---
+++ src/render.js
@@ -0,0 +1,25 @@
+//Setup
+const canvas = document.getElementById('canvas-1')
+const ctx1 = canvas.getContext('2d')
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas2 = document.getElementById('canvas-2')
+const ctx2 = canvas2.getContext('2d')
+canvas2.width = 600;
+canvas2.height = 600;
+
+const canvas3 = document.getElementById('canvas-3')
+const ctx3 = canvas3.getContext('2d')
+canvas3.width = 600;
+canvas3.height = 600;
+
+const canvas4 = document.getElementById('canvas-4')
+const ctx4 = canvas4.getContext('2d')
+canvas4.width = 600;
+canvas4.height = 600;
+
+const canvas5 = document.getElementById('canvas-5')
+const ctx5 = canvas5.getContext('2d')
+canvas5.width = 600;
+canvas5.height = 600;

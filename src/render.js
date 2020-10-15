2c2a52cc21709918c78524a91dd30a1408363aa3
tedu14 <thalison.eduardo14@gmail.com>
2020-10-15 17:36:34 -0300

    feat: add a initial obstacles functions

--- src/render.js
+++ src/render.js
@@ -152,3 +152,14 @@ class Obstacles{
         this.x += this.speed * gameSpeed;
     }
 }
+
+function initObstacles(){
+    // Lane 1
+    for(let i =0; i < 2; i++){
+        let x = i * 350;
+
+        carsArray.push(new Obstacles(x, canvas.height - grid * 2 - 20, grid, grid, 1, 'car'))
+    }
+}
+
+initObstacles()

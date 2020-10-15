366d6fb614be224c58a84349ba24772397f4f9ac
tedu14 <thalison.eduardo14@gmail.com>
2020-10-15 17:31:54 -0300

    feat: add a obstacles class

--- src/render.js
+++ src/render.js
@@ -132,3 +132,23 @@ window.addEventListener('keyup', (e) => {
 
     frogger.moving = false;
 })
+
+//Obstacles
+class Obstacles{
+    constructor(x, y, width, height, speed, type){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.type = type;
+    }
+
+    draw(){
+        ctx1.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    update(){
+        this.x += this.speed * gameSpeed;
+    }
+}
